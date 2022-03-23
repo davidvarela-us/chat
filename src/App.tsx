49b770e8c018ec06ccf36b28a396184b1474c514
David Varela <00.varela.david@gmail.com>
2022-03-23 23:01:59 +0000

    basic mobx API with array of string messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { createContext, useContext } from 'react';
 import './App.css';
+import { makeAutoObservable } from 'mobx';
+import { observer } from 'mobx-react-lite';
 
-console.log('websocket start');
-// Create WebSocket connection.
-const socket = new WebSocket('ws://localhost:4000');
+/* MOBX */
+class RootStore {
+    _socket: WebSocket | undefined = undefined;
+    _messages: string[] = [];
 
-// Connection opened
-socket.addEventListener('open', function (event) {
-    socket.send('Hello Server!');
-});
+    constructor() {
+        makeAutoObservable(this);
+    }
 
-// Listen for messages
-socket.addEventListener('message', function (event) {
-    console.log(`Message from server *${event.data}*`);
-});
-console.log('websocket end');
+    get messages() {
+        return this._messages;
+    }
 
-function App() {
+    pushMessage(x: string) {
+        console.log(`** new message: ${x}**`);
+        this._messages.push(x);
+        console.log(this.messages);
+    }
+
+    send(message: string) {
+        if (this._socket == null) {
+            console.log('cant send message with unconnected socket');
+            return undefined;
+        }
+
+        this.pushMessage(message);
+        this._socket.send(message);
+    }
+
+    connect() {
+        console.log('websocket start');
+
+        // Create WebSocket connection.
+        const socket = new WebSocket('ws://localhost:4000');
+
+        // Connection opened
+        socket.addEventListener('open', function (_) {
+            socket.send('Hello Server!');
+        });
+
+        // Listen for messages
+        socket.addEventListener('message', (event) => {
+            console.log(`Message from server *${event.data}*`);
+            this.pushMessage(event.data);
+        });
+
+        this._socket = socket;
+
+        console.log('websocket end');
+    }
+}
+
+/* APP */
+
+const RootStoreContext = createContext<RootStore>(new RootStore());
+
+const SendDialog: React.FC = observer(() => {
+    const store = useContext(RootStoreContext);
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload motherfucker.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+        <div>
+            <h1>Send a message</h1>
+            <button onClick={() => store.send('this is a message mothafucka')}>send</button>
         </div>
     );
-}
+});
+
+const store = new RootStore();
+store.connect();
+
+const App: React.FC = observer(() => {
+    return (
+        <RootStoreContext.Provider value={store}>
+            <SendDialog />
+            <div>
+                <h1> Messages </h1>
+                {store.messages
+                    .slice()
+                    .reverse()
+                    .map((x, i) => (
+                        <p key={i}>{x}</p>
+                    ))}
+            </div>
+        </RootStoreContext.Provider>
+    );
+});
 
 export default App;
