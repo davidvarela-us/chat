ad0d9d8ce6cfef7494f7f0f38e512de42fdda192
David Varela <00.varela.david@gmail.com>
2022-03-23 04:13:18 +0000

    basic websocket ping

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+console.log('websocket start');
+// Create WebSocket connection.
+const socket = new WebSocket('ws://localhost:4000');
+
+// Connection opened
+socket.addEventListener('open', function (event) {
+    socket.send('Hello Server!');
+});
+
+// Listen for messages
+socket.addEventListener('message', function (event) {
+    console.log(`Message from server *${event.data}*`);
+});
+console.log('websocket end');
+
 function App() {
     return (
         <div className="App">
