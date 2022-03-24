f63c12020f5a0514512d5b9edfb0463e9741d79c
David Varela <00.varela.david@gmail.com>
2022-03-24 23:58:26 +0000

    send userName in message, and format timestamp

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import './App.css';
 import { makeAutoObservable } from 'mobx';
 import { observer } from 'mobx-react-lite';
@@ -9,11 +9,13 @@ type Message = {
     message: string;
     timestamp: string;
     userID: string;
+    userName: string;
 };
 class RootStore {
     _socket: WebSocket | undefined = undefined;
     _messages: Message[] = [];
     _uuid: string;
+    _userName: string | undefined;
 
     constructor() {
         makeAutoObservable(this);
@@ -24,13 +26,31 @@ class RootStore {
         return this._messages;
     }
 
+    get userName() {
+        return this._userName;
+    }
+
+    // TODO why do I have to do this function signature?
+    set userName(userName: string | undefined) {
+        this._userName = userName;
+    }
+
+    get socket() {
+        return this._socket;
+    }
+
     pushMessage(x: Message) {
         this._messages.push(x);
     }
 
     send(message: string) {
         if (this._socket == null) {
-            console.log('cant send message with unconnected socket');
+            console.log('* cant send message with unconnected socket');
+            return undefined;
+        }
+
+        if (this._userName == null) {
+            console.log('* refusing to connect without username');
             return undefined;
         }
 
@@ -38,6 +58,7 @@ class RootStore {
             message: message,
             timestamp: String(Date.now()),
             userID: this._uuid,
+            userName: this._userName,
         };
 
         this.pushMessage(x);
@@ -45,6 +66,11 @@ class RootStore {
     }
 
     connect() {
+        if (this._userName == null) {
+            console.log('refusing to connect without username');
+            return undefined;
+        }
+
         console.log('websocket start');
 
         // Create WebSocket connection.
@@ -52,10 +78,16 @@ class RootStore {
 
         // Connection opened
         socket.addEventListener('open', (_) => {
+            if (this._userName == null) {
+                console.log('* refusing to connect without username');
+                return undefined;
+            }
+
             const x: Message = {
-                message: 'Hello Server!',
+                message: 'Hello server',
                 timestamp: String(Date.now()),
                 userID: this._uuid,
+                userName: this._userName,
             };
             socket.send(JSON.stringify(x));
         });
@@ -81,21 +113,57 @@ const RootStoreContext = createContext<RootStore>(new RootStore());
 
 const SendDialog: React.FC = observer(() => {
     const store = useContext(RootStoreContext);
+    const [message, setMessage] = useState('');
+
+    const submit = () => {
+        store.send(message);
+        setMessage('');
+    };
 
     return (
         <div>
             <h1>Send a message</h1>
-            <button onClick={() => store.send('this is a message mothafucka')}>send</button>
+            <input
+                onChange={(e) => setMessage(e.target.value)}
+                value={message}
+                onKeyPress={(e) => e.key === 'Enter' && submit()}
+                placeholder="message"
+            ></input>
+            <button onClick={submit}>Send</button>
         </div>
     );
 });
 
-const store = new RootStore();
-store.connect();
+const LoginScreen: React.FC = observer(() => {
+    const store = useContext(RootStoreContext);
 
-const App: React.FC = observer(() => {
+    const [userName, setUserName] = useState('');
+    const submit = () => {
+        store.userName = userName;
+        setUserName('');
+    };
+
     return (
-        <RootStoreContext.Provider value={store}>
+        <div>
+            <input
+                onChange={(e) => setUserName(e.target.value)}
+                value={userName}
+                onKeyPress={(e) => e.key === 'Enter' && submit()}
+                placeholder="Username"
+            ></input>
+            <button onClick={submit}>Enter</button>
+        </div>
+    );
+});
+
+const Chat: React.FC = observer(() => {
+    const store = useContext(RootStoreContext);
+    if (store.socket == null) {
+        store.connect();
+    }
+
+    return (
+        <div>
             <SendDialog />
             <div>
                 <h1> Messages </h1>
@@ -103,13 +171,27 @@ const App: React.FC = observer(() => {
                 {store.messages
                     .slice()
                     .reverse()
-                    .map((x, i) => (
-                        <div key={i} style={{ border: 'solid 1px gray', marginBottom: '1rem' }}>
-                            <h2>{x.message}</h2>
-                            <p>{`${x.userID} | ${x.timestamp}`}</p>
-                        </div>
-                    ))}
+                    .map((x, i) => {
+                        console.log('xxx', typeof x.timestamp, x.timestamp);
+                        return (
+                            <div key={i} style={{ border: 'solid 1px gray', marginBottom: '1rem' }}>
+                                <h2>{x.message}</h2>
+                                <p>{`${x.userName} | ${new Date(parseInt(x.timestamp)).toLocaleTimeString()}`}</p>
+                            </div>
+                        );
+                    })}
             </div>
+        </div>
+    );
+});
+
+const store = new RootStore();
+
+// TODO check username and that websocket connection is open before displaying chat?
+const App: React.FC = observer(() => {
+    return (
+        <RootStoreContext.Provider value={store}>
+            {store.userName == null ? <LoginScreen /> : <Chat />}
         </RootStoreContext.Provider>
     );
 });
